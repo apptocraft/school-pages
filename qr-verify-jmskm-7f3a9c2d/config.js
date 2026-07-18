1f8dd1f8ef2855a7bf7bcb3f17daac5226b79be3
apptocraft <anujitdas930@gmail.com>
2026-07-18 13:07:24 +0530

    Add files via upload

---
+++ qr-verify-jmskm-7f3a9c2d/config.js
@@ -0,0 +1,89 @@
+const CONFIG = {
+
+    school: {
+
+        name: "JAMIA MAHMUDIA SAWTUL KORAN MADRASA",
+
+        tagline: "",
+
+        address: `Bhurulia, Kaliganj, Plassey,
+        Nadia, West Bengal`,
+
+        logo: "logo.png",
+
+        info: [
+
+           {
+                label: "📑 Reg. No.",
+                value: "S/IL/54232"
+            },
+
+            {
+                label: "☎️ Mobile",
+                value: "7501479636 / 9933767534"
+            }
+
+        ]
+
+    },
+
+    verification: {
+
+        title: "Verification Successful",
+
+        message: "This student has been successfully verified as an enrolled student of {school}."
+
+    },
+
+
+    footer: {
+
+        text: "Powered by",
+
+        title: "{school}"
+
+    },
+
+    student: {
+
+        fields: [
+
+            {
+                key: "name",
+                label: "Student Name",
+            },
+
+            {
+                key: "class",
+                label: "Class",
+            },
+
+            {
+                key: "father",
+                label: "Father's Name",
+                icon: "👨"
+            },
+
+            {
+                key: "dob",
+                label: "Date of Birth",
+                icon: "🎂"
+            },
+
+            {
+                key: "mobile",
+                label: "Mobile Number",
+                icon: "📞"
+            },
+
+            {
+                key: "address",
+                label: "Address",
+                icon: "📍"
+            }
+
+        ]
+
+    }
+
+};
