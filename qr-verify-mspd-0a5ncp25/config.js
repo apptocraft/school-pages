cfd18df16e5fd1931dc78e8cde2d6bf7d541a0f0
apptocraft <anujitdas930@gmail.com>
2026-07-18 13:10:43 +0530

    Add files via upload

---
+++ qr-verify-mspd-0a5ncp25/config.js
@@ -0,0 +1,95 @@
+const CONFIG = {
+
+    school: {
+
+        name: "MODEL STUDY POINT",
+
+        tagline: "Empowering Students Since 2021",
+
+        address: `Debagram, Katwa More, Kaliganj
+        Nadia, West Bengal, 741137`,
+
+        logo: "logo.png",
+
+        info: [
+
+           {
+                label: "📞 Mobile",
+                value: "9647097306 / 9635706610"
+            },
+
+            {
+                label: "📧 Email",
+                value: "modelstudypoint@gmail.com"
+            }
+
+        ]
+
+    },
+
+    verification: {
+
+        title: "Verification Successful",
+
+        message: "This student has been successfully verified as an enrolled student of {school}."
+
+    },
+
+
+    footer: {
+
+        text: "Powered by",
+
+        title: "{school}"
+
+    },
+
+    student: {
+
+        fields: [
+
+            {
+                key: "name",
+                label: "Student Name",
+            },
+
+            {
+                key: "class",
+                label: "Class",
+            },
+
+            {
+                key: "id_no",
+                label: "ID No.",
+                icon: "🆔"
+            },
+
+            {
+                key: "father",
+                label: "Father's Name",
+                icon: "👨"
+            },
+
+            {
+                key: "dob",
+                label: "Date of Birth",
+                icon: "🎂"
+            },
+
+            {
+                key: "mobile",
+                label: "Mobile Number",
+                icon: "📞"
+            },
+
+            {
+                key: "address",
+                label: "Address",
+                icon: "📍"
+            }
+
+        ]
+
+    }
+
+};
